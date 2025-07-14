23aa1042b11c47c99c852ef38fa67bd3a1832e5b
jardelferreira <jardelferreira@gmail.com>
2025-07-14 16:52:33 -0300

    first commit

--- sw.js
+++ sw.js
@@ -6,15 +6,9 @@ const urlsToCache = [
   "./icon-192.png",
   "./icon-512.png",
   "./styles.css",
-  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/js/all.min.js",
-  "https://code.jquery.com/jquery-3.7.0.min.js",
-  "https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js",
+  "./spinner.png",
   "./operacoes.js",
   "./script.js",
-  "https://cdn.tailwindcss.com",
-  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css",
-  "https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css",
-  "https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap",
 ];
 
 self.addEventListener("install", event => {
